e2a5bb4cfe5a47b19ce5feab40274722a252dc20
pgh36 <pgh36@cornell.edu>
2021-05-13 10:51:22 -0400

    bayesian net runtime test added

--- kde/kernel.py
+++ kde/kernel.py
@@ -5,6 +5,4 @@ def gaussian_kernel(sigma_squared):
     def out(X):
         return (1.0 / np.sqrt(2 * np.pi * sigma_squared)) *\
             np.exp(-np.linalg.norm(X, axis = X.ndim - 1)**2 / (2 * sigma_squared))
-
-
     return out
